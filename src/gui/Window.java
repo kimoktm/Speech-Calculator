fb56c20ca7aa0f0f47a81815615c328e876ac1db
HeshamW <heshamwardany@gmail.com>
2014-11-23 19:17:10 +0200

    log and power works

--- src/gui/Window.java
+++ src/gui/Window.java
@@ -45,16 +45,14 @@ public class Window {
 	private void initialize() {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(Color.WHITE);
-		frame.setBounds(100, 100, 312, 300);
+		frame.setBounds(100, 100, 312, 332);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
 		final JTextArea txtrResult = new JTextArea();
 		txtrResult.setForeground(Color.WHITE);
 		txtrResult.setBackground(Color.BLACK);
-		txtrResult.setEnabled(false);
-		txtrResult.setEditable(false);
-		txtrResult.setBounds(12, 17, 288, 15);
+		txtrResult.setBounds(12, 12, 288, 15);
 		frame.getContentPane().add(txtrResult);
 
 		final JTextArea textArea = new JTextArea();
@@ -62,7 +60,7 @@ public class Window {
 		textArea.setBackground(Color.BLACK);
 		textArea.setEnabled(false);
 		textArea.setEditable(false);
-		textArea.setBounds(12, 245, 288, 15);
+		textArea.setBounds(12, 257, 288, 15);
 		frame.getContentPane().add(textArea);
 
 		JButton button = new JButton("1");
@@ -72,7 +70,7 @@ public class Window {
 			}
 		});
 		button.setBackground(Color.WHITE);
-		button.setBounds(12, 44, 61, 25);
+		button.setBounds(12, 39, 61, 25);
 		frame.getContentPane().add(button);
 
 		JButton button_1 = new JButton("2");
@@ -82,7 +80,7 @@ public class Window {
 			}
 		});
 		button_1.setBackground(Color.WHITE);
-		button_1.setBounds(85, 44, 61, 25);
+		button_1.setBounds(85, 39, 61, 25);
 		frame.getContentPane().add(button_1);
 
 		JButton button_2 = new JButton("3");
@@ -92,7 +90,7 @@ public class Window {
 			}
 		});
 		button_2.setBackground(Color.WHITE);
-		button_2.setBounds(158, 44, 61, 25);
+		button_2.setBounds(158, 39, 61, 25);
 		frame.getContentPane().add(button_2);
 
 		JButton button_3 = new JButton("4");
@@ -102,7 +100,7 @@ public class Window {
 			}
 		});
 		button_3.setBackground(Color.WHITE);
-		button_3.setBounds(12, 81, 61, 25);
+		button_3.setBounds(12, 76, 61, 25);
 		frame.getContentPane().add(button_3);
 
 		JButton button_4 = new JButton("5");
@@ -112,7 +110,7 @@ public class Window {
 			}
 		});
 		button_4.setBackground(Color.WHITE);
-		button_4.setBounds(85, 81, 61, 25);
+		button_4.setBounds(85, 76, 61, 25);
 		frame.getContentPane().add(button_4);
 
 		JButton button_5 = new JButton("6");
@@ -122,7 +120,7 @@ public class Window {
 			}
 		});
 		button_5.setBackground(Color.WHITE);
-		button_5.setBounds(158, 81, 61, 25);
+		button_5.setBounds(158, 76, 61, 25);
 		frame.getContentPane().add(button_5);
 
 		JButton button_6 = new JButton("7");
@@ -132,7 +130,7 @@ public class Window {
 			}
 		});
 		button_6.setBackground(Color.WHITE);
-		button_6.setBounds(12, 118, 61, 25);
+		button_6.setBounds(12, 113, 61, 25);
 		frame.getContentPane().add(button_6);
 
 		JButton button_7 = new JButton("8");
@@ -142,7 +140,7 @@ public class Window {
 			}
 		});
 		button_7.setBackground(Color.WHITE);
-		button_7.setBounds(85, 118, 61, 25);
+		button_7.setBounds(85, 113, 61, 25);
 		frame.getContentPane().add(button_7);
 
 		JButton button_8 = new JButton("9");
@@ -152,12 +150,17 @@ public class Window {
 			}
 		});
 		button_8.setBackground(Color.WHITE);
-		button_8.setBounds(158, 118, 61, 25);
+		button_8.setBounds(158, 113, 61, 25);
 		frame.getContentPane().add(button_8);
 
 		JButton btnE = new JButton("e");
+		btnE.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				txtrResult.setText(txtrResult.getText() + "exp");
+			}
+		});
 		btnE.setBackground(Color.WHITE);
-		btnE.setBounds(12, 155, 61, 25);
+		btnE.setBounds(12, 150, 61, 25);
 		frame.getContentPane().add(btnE);
 
 		JButton button_10 = new JButton("0");
@@ -167,12 +170,17 @@ public class Window {
 			}
 		});
 		button_10.setBackground(Color.WHITE);
-		button_10.setBounds(85, 155, 61, 25);
+		button_10.setBounds(85, 150, 61, 25);
 		frame.getContentPane().add(button_10);
 
 		JButton btnLog = new JButton("log");
+		btnLog.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				txtrResult.setText(txtrResult.getText() + "log b,expr");
+			}
+		});
 		btnLog.setBackground(Color.WHITE);
-		btnLog.setBounds(158, 155, 61, 25);
+		btnLog.setBounds(158, 150, 61, 25);
 		frame.getContentPane().add(btnLog);
 
 		JButton button_9 = new JButton("+");
@@ -182,7 +190,7 @@ public class Window {
 			}
 		});
 		button_9.setBackground(Color.WHITE);
-		button_9.setBounds(231, 44, 61, 25);
+		button_9.setBounds(231, 39, 61, 25);
 		frame.getContentPane().add(button_9);
 
 		JButton button_11 = new JButton("-");
@@ -192,7 +200,7 @@ public class Window {
 			}
 		});
 		button_11.setBackground(Color.WHITE);
-		button_11.setBounds(231, 81, 61, 25);
+		button_11.setBounds(231, 76, 61, 25);
 		frame.getContentPane().add(button_11);
 
 		JButton btnX = new JButton("x");
@@ -202,7 +210,7 @@ public class Window {
 			}
 		});
 		btnX.setBackground(Color.WHITE);
-		btnX.setBounds(231, 118, 61, 25);
+		btnX.setBounds(231, 113, 61, 25);
 		frame.getContentPane().add(btnX);
 
 		JButton button_13 = new JButton("/");
@@ -212,7 +220,7 @@ public class Window {
 			}
 		});
 		button_13.setBackground(Color.WHITE);
-		button_13.setBounds(231, 155, 61, 25);
+		button_13.setBounds(231, 150, 61, 25);
 		frame.getContentPane().add(button_13);
 
 		JButton btnRecord = new JButton("record");
@@ -222,7 +230,7 @@ public class Window {
 			}
 		});
 		btnRecord.setBackground(Color.WHITE);
-		btnRecord.setBounds(12, 193, 80, 25);
+		btnRecord.setBounds(12, 220, 280, 25);
 		frame.getContentPane().add(btnRecord);
 
 		JButton btnEnter = new JButton("=");
@@ -232,12 +240,15 @@ public class Window {
 						.getText());
 				if (result.equals("Syntax Error"))
 					textArea.setText(result);
-				else
+				else {
 					txtrResult.setText(result);
+					textArea.setText("");
+				}
+
 			}
 		});
 		btnEnter.setBackground(Color.WHITE);
-		btnEnter.setBounds(231, 192, 61, 25);
+		btnEnter.setBounds(231, 187, 61, 25);
 		frame.getContentPane().add(btnEnter);
 
 		JButton btnC = new JButton("C");
@@ -250,7 +261,7 @@ public class Window {
 			}
 		});
 		btnC.setBackground(Color.WHITE);
-		btnC.setBounds(158, 192, 61, 25);
+		btnC.setBounds(158, 187, 61, 25);
 		frame.getContentPane().add(btnC);
 
 		JButton button_12 = new JButton(".");
@@ -260,7 +271,17 @@ public class Window {
 			}
 		});
 		button_12.setBackground(Color.WHITE);
-		button_12.setBounds(104, 192, 42, 25);
+		button_12.setBounds(85, 187, 61, 25);
 		frame.getContentPane().add(button_12);
+
+		JButton button_14 = new JButton("^");
+		button_14.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				txtrResult.setText(txtrResult.getText() + "^");
+			}
+		});
+		button_14.setBackground(Color.WHITE);
+		button_14.setBounds(12, 187, 61, 25);
+		frame.getContentPane().add(button_14);
 	}
 }
